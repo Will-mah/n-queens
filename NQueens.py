77b4dbff07312d42486be50966ddced0fcd6f2eb
Will-Mah <wilmaop99@gmail.com>
2024-01-25 22:16:30 +0000

    python version

---
+++ NQueens.py
@@ -0,0 +1,51 @@
+class NQueens:
+    def __init__(self, n):
+        self.n = n
+        self.board = [-1] * n
+        self.cols = set()
+        self.diagonals = set()
+        self.antiDiagonals = set()
+        self.solutions = []
+
+    def is_safe(self, row, col):
+        return col not in self.cols and \
+               (row - col) not in self.diagonals and \
+               (row + col) not in self.antiDiagonals
+
+    def solve(self, row):
+        if row == self.n:
+            self.solutions.append(self.board.copy())
+            return True
+        for col in range(self.n):
+            if self.is_safe(row, col):
+                self.board[row] = col
+                self.cols.add(col)
+                self.diagonals.add(row - col)
+                self.antiDiagonals.add(row + col)
+                if self.solve(row + 1):
+                    return True
+                self.cols.remove(col)
+                self.diagonals.remove(row - col)
+                self.antiDiagonals.remove(row + col)
+        return False
+
+    def solve_n_queens(self):
+        self.solve(0)
+        return self.solutions
+
+def display_board(board):
+    for row in board:
+        line = ['.' for _ in range(len(board))]
+        line[row] = 'Q'
+        print(''.join(line))
+    print('-' * len(board) * 2)
+
+if __name__ == "__main__":
+    n = 25
+    solver = NQueens(n)
+    solutions = solver.solve_n_queens()
+    if solutions:
+        print(f"Found a solution for {n}-Queens problem python edition:")
+        display_board(solutions[0])
+    else:
+        print(f"No solutions found for {n}-Queens problem.")
